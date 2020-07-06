8659e25d58633a9406b8461f9c177e9d3cb51066
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2020-07-06 10:36:23 +0900

    deserialize in place

--- src/external_trait_impls/serde.rs
+++ src/external_trait_impls/serde.rs
@@ -81,6 +81,46 @@ where
         };
         deserializer.deserialize_seq(visitor)
     }
+
+    fn deserialize_in_place<D>(deserializer: D, place: &mut Self) -> Result<(), D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct VcInPlaceVisitor<'a, T>(&'a mut Vc<T>);
+        impl<'a, 'de, T> Visitor<'de> for VcInPlaceVisitor<'a, T>
+        where
+            T: Deserialize<'de>,
+        {
+            type Value = ();
+            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                formatter.write_str("a sequence")
+            }
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: SeqAccess<'de>,
+            {
+                let hint = helper::cautious_size_hint(seq.size_hint());
+                if let Some(additional) = hint.checked_sub(self.0.len()) {
+                    self.0.reserve(additional);
+                }
+                for i in 0..self.0.len() {
+                    let next = {
+                        let next_place = helper::InPlaceSeed(&mut self.0[i]);
+                        seq.next_element_seed(next_place)?
+                    };
+                    if next.is_none() {
+                        self.0.truncate(i);
+                        return Ok(());
+                    }
+                }
+                while let Some(value) = seq.next_element()? {
+                    self.0.push(value);
+                }
+                Ok(())
+            }
+        }
+        deserializer.deserialize_seq(VcInPlaceVisitor(place))
+    }
 }
 
 #[cfg(test)]
